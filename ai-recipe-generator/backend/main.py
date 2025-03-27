f96aa1a6766817d88f329fbe4addd8552d6b25d9
sagar-tan <tanwarsagarrobotics2274@gmail.com>
2025-03-28 03:23:28 +0530

    frontend and backend setup with fast API

--- ai-recipe-generator/backend/main.py
+++ ai-recipe-generator/backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Allow frontend to access backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins (change later for security)
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
 @app.get("/")
 def read_root():
-    return {"message": "Hello, FastAPI!"}
+    return {"message": "Hello from FastAPI"}
--- ai-recipe-generator/backend/venv/Scripts/activate.bat
+++ ai-recipe-generator/backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\tanwa\AI_recipie\ai-recipe-generator\backend\venv
+set VIRTUAL_ENV=C:\Users\tanwa\AI_Recipie\ai-recipe-generator\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
